6ebfbd57d0e6e395bb0576a2d984162bc6a82358
Mapse <mapse.b@hotmail.com>
2022-11-29 17:26:14 -0300

    Delete fit/condor directory
    Condor removed from wrong place

--- fit/condor/OniaOpenCharmRun2ULAna/quick_setup.sh
+++
@@ -1,8 +0,0 @@
-if [[ $_ == $0 ]]; then  
-  echo "This script is meant to be sourced:"
-  echo "  source $0"
-  exit 0
-fi
-
-conda activate OniaOpenCharmRun2ULenv
-voms-proxy-init --rfc --voms cms -valid 192:00
--- fit/condor/quick_setup.sh
+++
@@ -1,3 +0,0 @@
-voms-proxy-init --rfc --voms cms -valid 192:00
-cp /tmp/x509up_u128055 .
-
